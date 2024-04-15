54a4006a5b5a2c2a60068c22671b2d3a8c7af295
rudipsq <132471589+rudipsq@users.noreply.github.com>
2024-04-15 18:20:17 +0200

    v1.4.3

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", async function () {
-    document.body.style.overflowY = "visible";
+    // document.body.style.overflowY = "visible"; //TODO uncomment this line to enable website
 });
